d8f636f6344f0fdda8af84d05299276953a727e9
Anas Anas <Anas Anas>
2017-03-29 22:28:26 +0100

    Add pagination and fixed bugs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['angularUtils.directives.dirPagination']);
 
 app.controller('videoController', ['$scope', '$http', function ($scope, $http) {
+
     $http({
         method: 'GET',
         url: 'data.json'
@@ -11,4 +12,20 @@ app.controller('videoController', ['$scope', '$http', function ($scope, $http) {
     }, function errorCallback(response) {
         console.log(response);
     });
+
+
+    $scope.classForm = 'col-lg-3 filter';
+    $scope.classListItems = 'col-lg-9 list-items';
+    $scope.numberResults = 10;
+    $scope.hideForm = function (newPageNumber) {
+        if (newPageNumber > 1) {
+            $scope.classForm = 'hide';
+            $scope.classListItems = 'col-lg-12 list-items';
+            $scope.numberResults = 10;
+        } else {
+            $scope.classForm = 'col-lg-3 filter';
+            $scope.classListItems = 'col-lg-9 list-items';
+        }
+    };
+
 }]);
